75fd99be57d10c4011ca39ebac16631e6465f338
Do <du-1@qq.com>
2020-02-18 15:29:48 +0800

    更新bitmaputil 3

--- core/src/main/java/cn/dujc/core/util/BitmapUtil.java
+++ core/src/main/java/cn/dujc/core/util/BitmapUtil.java
@@ -81,7 +81,9 @@ public class BitmapUtil {
     @Nullable
     public static File createCompressedCacheFile(Context context, Uri uri, int sizeInKb, Bitmap.CompressFormat format) {
         if (context == null) return null;
-        File cacheFile = new File(context.getCacheDir(), System.currentTimeMillis() + (Bitmap.CompressFormat.PNG == format ? ".png" : Bitmap.CompressFormat.WEBP == format ? ".webp" : ".jpg"));
+        File cacheDir = context.getExternalCacheDir();
+        if (cacheDir == null) cacheDir = context.getCacheDir();
+        File cacheFile = new File(cacheDir, System.currentTimeMillis() + (Bitmap.CompressFormat.PNG == format ? ".png" : Bitmap.CompressFormat.WEBP == format ? ".webp" : ".jpg"));
         Bitmap bitmap = bitmapFromUri(context, uri);
         bitmap = BitmapUtil.shrinkImage(bitmap, sizeInKb, true);
         BitmapUtil.saveBitmapToFile(bitmap, cacheFile, format, 100, true);
@@ -110,7 +112,11 @@ public class BitmapUtil {
         if (context == null || uri == null) return null;
         try {
             ParcelFileDescriptor parcelFileDescriptor = context.getContentResolver().openFileDescriptor(uri, "r");
-            return BitmapFactory.decodeFileDescriptor(parcelFileDescriptor.getFileDescriptor());
+            Bitmap bitmap = BitmapFactory.decodeFileDescriptor(parcelFileDescriptor.getFileDescriptor());
+            InputStream stream = readUri(context, uri);
+            int orientation = getOrientation(stream);
+            bitmap = rotateBitmap(bitmap, orientation);
+            return bitmap;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -328,6 +334,171 @@ public class BitmapUtil {
         newOpts = null;
         return bitmap;
     }
+
+    /**
+     * 读取图片属性：旋转的角度
+     *
+     * @param uri 图片
+     * @return degree旋转的角度
+     */
+    @Nullable
+    public static InputStream readUri(Context context, Uri uri) {
+        try {
+            return context.getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            if (Core.DEBUG) e.printStackTrace();
+        }
+        return null;
+    }
+
+    // -------------------------- 来自Luban --------------------------
+
+    /**
+     * 读取图片属性：旋转的角度
+     *
+     * @return degree旋转的角度
+     */
+    public static int getOrientation(InputStream is) {
+        return getOrientation(toByteArray(is));
+    }
+
+    private static byte[] toByteArray(InputStream is) {
+        if (is == null) {
+            return new byte[0];
+        }
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+
+        int read;
+        byte[] data = new byte[4096];
+
+        try {
+            while ((read = is.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, read);
+            }
+        } catch (Exception ignored) {
+            return new byte[0];
+        } finally {
+            try {
+                buffer.close();
+            } catch (IOException ignored) {
+            }
+        }
+
+        return buffer.toByteArray();
+    }
+
+    private static int getOrientation(byte[] jpeg) {
+        if (jpeg == null) {
+            return 0;
+        }
+
+        int offset = 0;
+        int length = 0;
+
+        // ISO/IEC 10918-1:1993(E)
+        while (offset + 3 < jpeg.length && (jpeg[offset++] & 0xFF) == 0xFF) {
+            int marker = jpeg[offset] & 0xFF;
+
+            // Check if the marker is a padding.
+            if (marker == 0xFF) {
+                continue;
+            }
+            offset++;
+
+            // Check if the marker is SOI or TEM.
+            if (marker == 0xD8 || marker == 0x01) {
+                continue;
+            }
+            // Check if the marker is EOI or SOS.
+            if (marker == 0xD9 || marker == 0xDA) {
+                break;
+            }
+
+            // Get the length and check if it is reasonable.
+            length = pack(jpeg, offset, 2, false);
+            if (length < 2 || offset + length > jpeg.length) {
+                LogUtil.e("Invalid length");
+                return 0;
+            }
+
+            // Break if the marker is EXIF in APP1.
+            if (marker == 0xE1 && length >= 8
+                    && pack(jpeg, offset + 2, 4, false) == 0x45786966
+                    && pack(jpeg, offset + 6, 2, false) == 0) {
+                offset += 8;
+                length -= 8;
+                break;
+            }
+
+            // Skip other markers.
+            offset += length;
+            length = 0;
+        }
+
+        // JEITA CP-3451 Exif Version 2.2
+        if (length > 8) {
+            // Identify the byte order.
+            int tag = pack(jpeg, offset, 4, false);
+            if (tag != 0x49492A00 && tag != 0x4D4D002A) {
+                LogUtil.e("Invalid byte order");
+                return 0;
+            }
+            boolean littleEndian = (tag == 0x49492A00);
+
+            // Get the offset and check if it is reasonable.
+            int count = pack(jpeg, offset + 4, 4, littleEndian) + 2;
+            if (count < 10 || count > length) {
+                LogUtil.e("Invalid offset");
+                return 0;
+            }
+            offset += count;
+            length -= count;
+
+            // Get the count and go through all the elements.
+            count = pack(jpeg, offset - 2, 2, littleEndian);
+            while (count-- > 0 && length >= 12) {
+                // Get the tag and check if it is orientation.
+                tag = pack(jpeg, offset, 2, littleEndian);
+                if (tag == 0x0112) {
+                    int orientation = pack(jpeg, offset + 8, 2, littleEndian);
+                    switch (orientation) {
+                        case 1:
+                            return 0;
+                        case 3:
+                            return 180;
+                        case 6:
+                            return 90;
+                        case 8:
+                            return 270;
+                    }
+                    LogUtil.e("Unsupported orientation");
+                    return 0;
+                }
+                offset += 12;
+                length -= 12;
+            }
+        }
+
+        LogUtil.e("Orientation not found");
+        return 0;
+    }
+
+    private static int pack(byte[] bytes, int offset, int length, boolean littleEndian) {
+        int step = 1;
+        if (littleEndian) {
+            offset += length - 1;
+            step = -1;
+        }
+
+        int value = 0;
+        while (length-- > 0) {
+            value = (value << 8) | (bytes[offset] & 0xFF);
+            offset += step;
+        }
+        return value;
+    }
+    // -------------------------- 来自Luban --------------------------
 
     /**
      * 读取图片属性：旋转的角度
