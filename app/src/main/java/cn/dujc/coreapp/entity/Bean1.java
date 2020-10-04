2003733192d9c26702ebb131b0999abc8c952d01
jianchi <727809864@qq.com>
2020-10-04 18:49:06 +0800

    似乎完成了类型错误页不崩溃的gson实现2

--- app/src/main/java/cn/dujc/coreapp/entity/Bean1.java
+++ app/src/main/java/cn/dujc/coreapp/entity/Bean1.java
@@ -2,211 +2,61 @@ package cn.dujc.coreapp.entity;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
-public class Bean1 extends Bean<String> implements Serializable {
-    private static final long serialVersionUID = -3824251436674975025L;
+public class Bean1 implements Serializable {
+    private int[] intArrays;
+    private String[] stringArrays;
+    private boolean[] booleanArrays;
+    private List<Float> floatList;
+    private List<String> stringList;
+    private Map<String, String> stringStringMap;
 
-    /**
-     * id : 1644533
-     * otype : 5
-     * is_print : 1
-     * read_status : 1
-     * orders_status : 4
-     * complete_time : 2018-08-31
-     * apply_read_status : 0
-     * test_name : 啦啦啦
-     * test_phone : 15222222222
-     * test_sex : 男
-     * orders_code : 153569989591594
-     * is_read : 1
-     * report_url :
-     * barcode : 1406134564
-     * baonum : 1
-     * status : 1
-     * pdf : []
-     * pnames_str : 血清绒毛膜促性腺激素测定(HCG) 
-     * item_result_hint : 正常
-     * item_result_en : 1
-     */
-
-    private String id;
-    private String otype;
-    private String is_print;
-    private String read_status;
-    private String orders_status;
-    private String complete_time;
-    private String apply_read_status;
-    private String test_name;
-    private String test_phone;
-    private String test_sex;
-    private String orders_code;
-    private String is_read;
-    private String report_url;
-    private String barcode;
-    private String baonum;
-    private String status;
-    private String pnames_str;
-    private String item_result_hint;
-    private int item_result_en;
-    private List<String> pdf;
-
-    public String getId() {
-        return id;
+    public int[] getIntArrays() {
+        return intArrays;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setIntArrays(int[] intArrays) {
+        this.intArrays = intArrays;
     }
 
-    public String getOtype() {
-        return otype;
+    public String[] getStringArrays() {
+        return stringArrays;
     }
 
-    public void setOtype(String otype) {
-        this.otype = otype;
+    public void setStringArrays(String[] stringArrays) {
+        this.stringArrays = stringArrays;
     }
 
-    public String getIs_print() {
-        return is_print;
+    public boolean[] getBooleanArrays() {
+        return booleanArrays;
     }
 
-    public void setIs_print(String is_print) {
-        this.is_print = is_print;
+    public void setBooleanArrays(boolean[] booleanArrays) {
+        this.booleanArrays = booleanArrays;
     }
 
-    public String getRead_status() {
-        return read_status;
+    public List<Float> getFloatList() {
+        return floatList;
     }
 
-    public void setRead_status(String read_status) {
-        this.read_status = read_status;
+    public void setFloatList(List<Float> floatList) {
+        this.floatList = floatList;
     }
 
-    public String getOrders_status() {
-        return orders_status;
+    public List<String> getStringList() {
+        return stringList;
     }
 
-    public void setOrders_status(String orders_status) {
-        this.orders_status = orders_status;
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
     }
 
-    public String getComplete_time() {
-        return complete_time;
+    public Map<String, String> getStringStringMap() {
+        return stringStringMap;
     }
 
-    public void setComplete_time(String complete_time) {
-        this.complete_time = complete_time;
-    }
-
-    public String getApply_read_status() {
-        return apply_read_status;
-    }
-
-    public void setApply_read_status(String apply_read_status) {
-        this.apply_read_status = apply_read_status;
-    }
-
-    public String getTest_name() {
-        return test_name;
-    }
-
-    public void setTest_name(String test_name) {
-        this.test_name = test_name;
-    }
-
-    public String getTest_phone() {
-        return test_phone;
-    }
-
-    public void setTest_phone(String test_phone) {
-        this.test_phone = test_phone;
-    }
-
-    public String getTest_sex() {
-        return test_sex;
-    }
-
-    public void setTest_sex(String test_sex) {
-        this.test_sex = test_sex;
-    }
-
-    public String getOrders_code() {
-        return orders_code;
-    }
-
-    public void setOrders_code(String orders_code) {
-        this.orders_code = orders_code;
-    }
-
-    public String getIs_read() {
-        return is_read;
-    }
-
-    public void setIs_read(String is_read) {
-        this.is_read = is_read;
-    }
-
-    public String getReport_url() {
-        return report_url;
-    }
-
-    public void setReport_url(String report_url) {
-        this.report_url = report_url;
-    }
-
-    public String getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
-    public String getBaonum() {
-        return baonum;
-    }
-
-    public void setBaonum(String baonum) {
-        this.baonum = baonum;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPnames_str() {
-        return pnames_str;
-    }
-
-    public void setPnames_str(String pnames_str) {
-        this.pnames_str = pnames_str;
-    }
-
-    public String getItem_result_hint() {
-        return item_result_hint;
-    }
-
-    public void setItem_result_hint(String item_result_hint) {
-        this.item_result_hint = item_result_hint;
-    }
-
-    public int getItem_result_en() {
-        return item_result_en;
-    }
-
-    public void setItem_result_en(int item_result_en) {
-        this.item_result_en = item_result_en;
-    }
-
-    public List<String> getPdf() {
-        return pdf;
-    }
-
-    public void setPdf(List<String> pdf) {
-        this.pdf = pdf;
+    public void setStringStringMap(Map<String, String> stringStringMap) {
+        this.stringStringMap = stringStringMap;
     }
 }
--- app/src/main/java/cn/dujc/coreapp/entity/Bean2.java
+++ app/src/main/java/cn/dujc/coreapp/entity/Bean2.java
@@ -2,255 +2,61 @@ package cn.dujc.coreapp.entity;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 public class Bean2 implements Serializable {
-    private static final long serialVersionUID = 6603103664641364959L;
+    private double[] intArrays;
+    private String stringArrays;
+    private boolean booleanArrays;
+    private List<Integer> floatList;
+    private List<Float> stringList;
+    private Map<String, Integer> stringStringMap;
 
-    /**
-     * id : 1644533
-     * otype : 5
-     * is_print : 1
-     * read_status : 1
-     * orders_status : 4
-     * complete_time : 2018-08-31
-     * apply_read_status : 0
-     * test_name : 啦啦啦
-     * test_phone : 15222222222
-     * test_sex : 男
-     * orders_code : 153569989591594
-     * is_read : 1
-     * report_url :
-     * barcode : 1406134564
-     * baonum : 1
-     * status : 1
-     * pdf : []
-     * pnames_str : 血清绒毛膜促性腺激素测定(HCG) 
-     * item_result_hint : 正常
-     * item_result_en : 1
-     */
-
-    private int id;
-    private Integer otype;
-    private boolean is_print;
-    private double read_status;
-    private float orders_status;
-    private String complete_time;
-    private Double apply_read_status;
-    private String test_name;
-    private Float test_phone;
-    private byte test_sex;
-    private String orders_code;
-    private Byte is_read;
-    private Long report_url;
-    private long barcode;
-    private int[] baonum;
-    private String[] status;
-    private Data pnames_str;
-    private Object item_result_hint;
-    private int item_result_en;
-    private List<String> pdf;
-    private Data2<String> data2;
-
-    public static class Data2<T> implements Serializable {
-        private static final long serialVersionUID = -4566920785569602019L;
-        private T obj;
-
-        public T getObj() {
-            return obj;
-        }
-
-        public void setObj(T obj) {
-            this.obj = obj;
-        }
+    public double[] getIntArrays() {
+        return intArrays;
     }
 
-    public static class Data implements Serializable {
-        private static final long serialVersionUID = 1170611118398679438L;
-        private String test;
-        private Object obj;
-
-        public String getTest() {
-            return test;
-        }
-
-        public void setTest(String test) {
-            this.test = test;
-        }
-
-        public Object getObj() {
-            return obj;
-        }
-
-        public void setObj(Object obj) {
-            this.obj = obj;
-        }
+    public void setIntArrays(double[] intArrays) {
+        this.intArrays = intArrays;
     }
 
-    public int getId() {
-        return id;
+    public String getStringArrays() {
+        return stringArrays;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setStringArrays(String stringArrays) {
+        this.stringArrays = stringArrays;
     }
 
-    public Integer getOtype() {
-        return otype;
+    public boolean isBooleanArrays() {
+        return booleanArrays;
     }
 
-    public void setOtype(Integer otype) {
-        this.otype = otype;
+    public void setBooleanArrays(boolean booleanArrays) {
+        this.booleanArrays = booleanArrays;
     }
 
-    public boolean isIs_print() {
-        return is_print;
+    public List<Integer> getFloatList() {
+        return floatList;
     }
 
-    public void setIs_print(boolean is_print) {
-        this.is_print = is_print;
+    public void setFloatList(List<Integer> floatList) {
+        this.floatList = floatList;
     }
 
-    public double getRead_status() {
-        return read_status;
+    public List<Float> getStringList() {
+        return stringList;
     }
 
-    public void setRead_status(double read_status) {
-        this.read_status = read_status;
+    public void setStringList(List<Float> stringList) {
+        this.stringList = stringList;
     }
 
-    public float getOrders_status() {
-        return orders_status;
+    public Map<String, Integer> getStringStringMap() {
+        return stringStringMap;
     }
 
-    public void setOrders_status(float orders_status) {
-        this.orders_status = orders_status;
-    }
-
-    public String getComplete_time() {
-        return complete_time;
-    }
-
-    public void setComplete_time(String complete_time) {
-        this.complete_time = complete_time;
-    }
-
-    public Double getApply_read_status() {
-        return apply_read_status;
-    }
-
-    public void setApply_read_status(Double apply_read_status) {
-        this.apply_read_status = apply_read_status;
-    }
-
-    public String getTest_name() {
-        return test_name;
-    }
-
-    public void setTest_name(String test_name) {
-        this.test_name = test_name;
-    }
-
-    public Float getTest_phone() {
-        return test_phone;
-    }
-
-    public void setTest_phone(Float test_phone) {
-        this.test_phone = test_phone;
-    }
-
-    public byte getTest_sex() {
-        return test_sex;
-    }
-
-    public void setTest_sex(byte test_sex) {
-        this.test_sex = test_sex;
-    }
-
-    public String getOrders_code() {
-        return orders_code;
-    }
-
-    public void setOrders_code(String orders_code) {
-        this.orders_code = orders_code;
-    }
-
-    public Byte getIs_read() {
-        return is_read;
-    }
-
-    public void setIs_read(Byte is_read) {
-        this.is_read = is_read;
-    }
-
-    public Long getReport_url() {
-        return report_url;
-    }
-
-    public void setReport_url(Long report_url) {
-        this.report_url = report_url;
-    }
-
-    public long getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(long barcode) {
-        this.barcode = barcode;
-    }
-
-    public int[] getBaonum() {
-        return baonum;
-    }
-
-    public void setBaonum(int[] baonum) {
-        this.baonum = baonum;
-    }
-
-    public String[] getStatus() {
-        return status;
-    }
-
-    public void setStatus(String[] status) {
-        this.status = status;
-    }
-
-    public Data getPnames_str() {
-        return pnames_str;
-    }
-
-    public void setPnames_str(Data pnames_str) {
-        this.pnames_str = pnames_str;
-    }
-
-    public Object getItem_result_hint() {
-        return item_result_hint;
-    }
-
-    public void setItem_result_hint(Object item_result_hint) {
-        this.item_result_hint = item_result_hint;
-    }
-
-    public int getItem_result_en() {
-        return item_result_en;
-    }
-
-    public void setItem_result_en(int item_result_en) {
-        this.item_result_en = item_result_en;
-    }
-
-    public List<String> getPdf() {
-        return pdf;
-    }
-
-    public void setPdf(List<String> pdf) {
-        this.pdf = pdf;
-    }
-
-    public Data2<String> getData2() {
-        return data2;
-    }
-
-    public void setData2(Data2<String> data2) {
-        this.data2 = data2;
+    public void setStringStringMap(Map<String, Integer> stringStringMap) {
+        this.stringStringMap = stringStringMap;
     }
 }
